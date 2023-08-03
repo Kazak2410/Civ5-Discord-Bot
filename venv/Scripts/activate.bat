49f7b64d5c42ceddf8c454002b1a58250b02c6ff
Ilya <ballukrainian@gmail.com>
2023-08-03 11:16:44 +0300

    bug fixes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Civ5_discord_bot\venv
+set VIRTUAL_ENV=D:\projects\Civ5_discord_bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
